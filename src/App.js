6b13ecc53ac8b168f65cebe0f033368f5dca6d4f
Milan Zivkovic <zivkovicmilan1987@gmail.com>
2022-09-04 10:34:55 -0500

    done with useMemo hook

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState, useEffect, useMemo} from 'react'
+import styles from "./App.module.css"
+
+
 
 function App() {
+const [number, setNumber] = useState(0)
+const [dark, setDark] = useState(false)
+
+// * 2. use to check for refenecial equality for object and arrays
+const themeStyles = useMemo(()=> {
+  return {
+    backgroundColor: dark ? "black" : "white",
+    color: dark ? "white" : "black"
+  }}, [dark])
+
+
+
+const inputHandler = (event) => {
+  console.log("hello")
+  setNumber(event.target.value)
+}
+ 
+// * 1. use to run log tasks only in specific situations and not on every re-render
+const doubledNumber = useMemo( ()=> slowFunction(number), [number])
+
+useEffect(()=>{
+  console.log("change theme running")
+}, [themeStyles])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1 className={styles.heading}>useMemo Hook Practice</h1>
+      <input type="number" value={number} onChange={inputHandler} />
+      <button onClick={() => setDark((prevDark) => !prevDark)}>Change Theme</button>
+      <div style={themeStyles}>{doubledNumber}</div>
     </div>
+    
   );
+}
+
+function slowFunction(num){
+  for(let i=0; i< 2000000000; i++){}
+  return num * 2
 }
 
 export default App;
